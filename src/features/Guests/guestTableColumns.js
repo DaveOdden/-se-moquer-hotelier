db08ae3e8a1b492404ff51e0281216d504ca6a93
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-10-15 21:33:15 -0400

    move new guest form to component

---
+++ src/features/Guests/guestTableColumns.js
@@ -0,0 +1,40 @@
+export const columnDefinitions = [
+  {
+    title: 'Last Name',
+    dataIndex: 'lastName',
+    width: '200px',
+    key: 'lastName',
+    paddingLeft: '50px'
+  },
+  {
+    title: 'First Name',
+    dataIndex: 'firstName',
+    key: 'firstName',
+    width: '200px',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Street Name',
+    dataIndex: 'street',
+    key: 'street',
+  },
+  {
+    title: 'City',
+    dataIndex: 'city',
+    key: 'city',
+  },
+  {
+    title: 'State',
+    dataIndex: 'state',
+    key: 'state',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+  },
+];
