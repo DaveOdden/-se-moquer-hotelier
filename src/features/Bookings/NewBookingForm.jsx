35f6a7edf6f5f439e28a60bd612e929aa20a7e8a
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-11-01 17:50:08 -0400

    disable dates in checkin date picker based on selected room

--- src/features/Bookings/NewBookingForm.jsx
+++ src/features/Bookings/NewBookingForm.jsx
@@ -1,29 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { GuestAPI } from '../../api/GuestAPI'
 import { RoomsAPI } from '../../api/RoomAPI'
+import { BookingsByRoomAPI } from '../../api/BookingsAPI'
 import { AutoComplete, Space, Form, Input, Button, DatePicker, TimePicker } from 'antd';
 import dayjs from 'dayjs';
 
 export default function NewBookingForm(props) {
   const [guests, setGuests] = useState([]);
+  const [guestsKeyValueSet, setGuestsForAutoComplete] = useState([]);
   const [rooms, setRooms] = useState([]);
+  const [arrayOfDatesRoomIsBooked, setArrayOfDatesRoomIsBooked] = useState([]);
+  const [bookingsForSelectedRoom, setBookingsForSelectedRoom] = useState([]);
+  const [roomsKeyValueSet, setRoomsForAutoComplete] = useState([]);
   const [guestIsLoading, setGuestLoadingState] = useState(true);
-  const [roomIsLoading, setRoomLoadingState] = useState(true);
+  const [roomIsLoading, setRoomLoadingState] = useState(true);;
   const format = 'h:mm a';
+  const dates = ['2023-10-23', '2023-10-19']
 
   const getGuestData = () => {
-    GuestAPI.get().then((res) => {
+    GuestAPI.get('keyvaluepair').then((res) => {
       setGuests(res.message)
+      setGuestsForAutoComplete(res.keyvalpair)
       setGuestLoadingState(false)
     })
   }
 
   const getRoomData = () => {
-    RoomsAPI.get().then((res) => {
-      console.log(res.message);
-      setRooms(res.message)
+    RoomsAPI.get('keyvaluepair').then((res) => {
+      setRooms(res.rooms)
+      setRoomsForAutoComplete(res.keyvalpair)
       setRoomLoadingState(false)
     })
+  }
+
+  const getBookingData = (roomId) => {
+    BookingsByRoomAPI.get('id', roomId).then((res) => {
+      console.log(res.message)
+      setBookingsForSelectedRoom(res.message.bookings);
+      setArrayOfDatesRoomIsBooked(res.message.datesBooked)
+    })
+  }
+
+  const getRoomAvailability = (id) => {
+    getBookingData(id);
   }
 
   const init = () => {
@@ -33,26 +52,42 @@ export default function NewBookingForm(props) {
 
   useEffect(() => init, [])
 
+  const findDatesToDisable = (current) => {
+    let index = arrayOfDatesRoomIsBooked.findIndex(date => date === dayjs(current).format('YYYY-MM-DD'))
+    return index > -1 && true
+  }
+
   return (
-    <Form id="bookingForm" onFinish={props.submitFn}>
-      <Form.Item name="name" label="Guest's Name">
+
+      <Form id="bookingForm" onFinish={props.submitFn}>
+      <Form.Item name="name" label="Guest">
         <AutoComplete
-          options={[{label: 'label', value: 'value'}]}
+          options={guestsKeyValueSet}
+          filterOption={true}
           placeholder="Search Guests"
         />
       </Form.Item>
       <Form.Item name="room" label="Room">
         <AutoComplete
-          options={[{label: 'label', value: 'value'}]}
-          placeholder="Search Guests"
+          options={roomsKeyValueSet}
+          filterOption={true}
+          placeholder="Search Rooms"
+          onChange={getRoomAvailability}
         />
       </Form.Item>
       <Space>
         <Form.Item name="checkinDate" label="CheckIn Date">
-          <DatePicker />
+          <DatePicker 
+            disabledDate={findDatesToDisable}
+          />
         </Form.Item>
         <Form.Item name="checkinTime" label="Checkin Time">
-          <TimePicker use12Hours minuteStep={15} defaultValue={dayjs('10:30', 'HH:mm')} format="h:mm a" />
+          <TimePicker 
+            use12Hours 
+            minuteStep={15} 
+            defaultValue={dayjs('12:00', 'HH:mm')} 
+            format="h:mm a"
+          />
         </Form.Item>
       </Space>
       <Space>
@@ -60,7 +95,12 @@ export default function NewBookingForm(props) {
           <DatePicker />
         </Form.Item>
         <Form.Item name="checkoutTime" label="Checkout Time">
-          <TimePicker use12Hours minuteStep={15} defaultValue={dayjs('10:30', 'HH:mm')} format="h:mm a" />
+          <TimePicker 
+            use12Hours 
+            minuteStep={15} 
+            defaultValue={dayjs('10:30', 'HH:mm')} 
+            format="h:mm a" 
+          />
         </Form.Item>
       </Space>
       <Form.Item label="Payment">
@@ -68,10 +108,11 @@ export default function NewBookingForm(props) {
       </Form.Item>
 
       <Form.Item>
-        <Button  type="primary" key="submit" htmlType="submit">
+        <Button type="primary" key="submit" htmlType="submit">
           Submit
         </Button>
       </Form.Item>
     </Form>
+
   )
 }
