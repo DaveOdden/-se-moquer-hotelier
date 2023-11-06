d6455a260c9178d851e67b1015ffaf349f608835
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-11-05 19:08:45 -0500

    new booking form with confirmation

--- src/features/Bookings/Bookings.jsx
+++ src/features/Bookings/Bookings.jsx
@@ -69,6 +69,7 @@ export default function Bookings() {
         <Table 
           dataSource={bookings} 
           columns={columns} 
+          loading={isLoading}
           size="middle"
           rowKey={(record) => record._id}
         />
