dbcf263b317490a6f32b3e11f7d0c3183f35cc01
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-11-18 19:04:03 -0500

    better presentation of details with Calendar component

--- src/features/Bookings/BookingDetail.jsx
+++ src/features/Bookings/BookingDetail.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { Drawer, Descriptions, Button, Space, Dropdown } from 'antd'
-import { MoreOutlined, DeleteOutlined, EditOutlined, LineOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react'
+import { Drawer, Descriptions, Button, Space, Dropdown, Divider, Calendar } from 'antd'
+import { MoreOutlined, DeleteOutlined, EditOutlined, LineOutlined } from '@ant-design/icons'
+import { getArrayOfDatesBooked, findDatesToDisable } from 'src/utils/dateHelpers'
 
 export const BookingDetail = (props) => {
-  const [isOpen, setOpenState] = useState(false);
-  const [isEditing, setEditState] = useState(false);
-  const [descriptionContent, setDescriptionContent] = useState(null);
-  const [editGuestFormStatus, setEditGuestFormStatus] = useState({ loading: false, response: null, error: null, pristine: true});
+  const { data, show, deleteBooking } = props
+  const [isOpen, setOpenState] = useState(false)
+  const [isEditing, setEditState] = useState(false)
+  const [datesBooked, setDatesBooked] = useState(null)
 
   const actionItems = [
     {
@@ -32,7 +33,7 @@ export const BookingDetail = (props) => {
         <DeleteOutlined />
       ),
       danger: true,
-      onClick: () => props.deleteBooking(props.data._id)
+      onClick: () => deleteBooking(data._id)
     },
   ]
   
@@ -42,36 +43,58 @@ export const BookingDetail = (props) => {
     props.onClose();
   };
 
-  const transformDataForDescription = () => {
-    let descriptionContent = [];
-    function loopOverProperties(dataObj) {
-      let index = 0;
-      for(const key in dataObj) {
-        if(typeof dataObj[key] != "object") {
-          descriptionContent.push({
-            key: index,
-            label: key,
-            children: dataObj[key]
-          })
-          index++
-        } else {
-          loopOverProperties(dataObj[key])
-        }
-      }
-    }
-    if(props.data) {
-      loopOverProperties(props.data)
-      setDescriptionContent(descriptionContent);
+  const init = () => {
+    if(data) {
+      let bookedDates = getArrayOfDatesBooked(data.checkinDate, data.checkoutDate)
+      setDatesBooked(bookedDates)
     }
   }
 
-  useEffect(() => {
-    transformDataForDescription()
-  }, [props.data]);
+  const disableCalendarDates = (current) => {
+    return findDatesToDisable(datesBooked, current)
+  }
+
+  const otherBookedDates = []; // Replace with room's booked dates
+
+  const isOtherBooking = (currentDate) => {
+    const formattedDate = currentDate.format('YYYY-MM-DD');
+    return otherBookedDates.includes(formattedDate);
+  }
+
+  const thisBooking = (currentDate) => {
+    const formattedDate = currentDate.format('YYYY-MM-DD');
+    return datesBooked.includes(formattedDate);
+  }
+
+  const borderRadiusForSeries = (currentDate) => {
+    let tomorrow = currentDate.add(1, 'day').format('YYYY-MM-DD');
+    let yesterday = currentDate.subtract(1, 'day').format('YYYY-MM-DD');
+
+    if(!datesBooked.includes(yesterday)) {
+      return '10px 0 0 10px'
+    }
+
+    if(!datesBooked.includes(tomorrow)) {
+      return '0 10px 10px 0'
+    }
+    return 'none'
+  }
+
+  const dateCellRender = (currentDate) => {
+    if (isOtherBooking(currentDate)) {
+      return <div style={{background: '#eee'}}>{currentDate.date()}</div>;
+    }
+    if (thisBooking(currentDate)) {
+      return <div style={{background: '#1677ff', color: 'white', borderRadius: borderRadiusForSeries(currentDate)}}>{currentDate.date()}</div>;
+    }
+    return currentDate.date();
+  }
 
   useEffect(() => {
-    setOpenState(props.show)
-  }, [props.show]);
+    setOpenState(show)
+  }, [show]);
+
+  useEffect(() => init(), [data])
 
   return (
     <>
@@ -100,11 +123,45 @@ export const BookingDetail = (props) => {
           </Space>
         }>
         { !isEditing && 
-          <Descriptions 
-            items={descriptionContent} 
-            column={1} 
-            layout="small" 
-            contentStyle={{textAlign: 'left'}} />
+          <>
+            <Descriptions 
+              size="small"
+              style={{
+                textAlign: 'left',
+                marginTop: '0', 
+              }} 
+              column={1}
+              labelStyle={{
+                width: '120px'
+              }}>
+              { !data?.paid && <Descriptions.Item label="Paid">Pending Payment</Descriptions.Item> }
+              <Descriptions.Item label="Room">#{data?.room?._id}</Descriptions.Item>
+              <Descriptions.Item label="Guest Name">{data?.guest?.firstName} {data?.guest?.lastName}</Descriptions.Item>
+              <Descriptions.Item label="Confirmation #">{data?._id}</Descriptions.Item>
+            </Descriptions>
+            <Divider style={{marginTop: '12px'}} />
+            <Calendar 
+              mode="month" 
+              fullscreen={false} 
+              fullCellRender={dateCellRender}
+              style={{marginTop: '-20px'}} />
+            <Divider />
+            <Descriptions 
+              size="small"
+              style={{
+                textAlign: 'left',
+                marginTop: '0', 
+              }} 
+              column={1}
+              labelStyle={{
+                width: '120px'
+              }}>
+              <Descriptions.Item label="Duration of Stay">{data?.billing?.days} days</Descriptions.Item>
+              <Descriptions.Item label="Check-in">{data?.checkinDate}</Descriptions.Item>
+              <Descriptions.Item label="Check-out">{data?.checkoutDate}</Descriptions.Item>
+              <Divider style={{marginTop: '-12px'}} />
+            </Descriptions>
+          </>
         }
       </Drawer>
     </>
